2554245bab6f6f4fc0dd357274bf4f4b5d5494d2
Muniir1 <muniirm81@gmail.com>
2024-01-14 20:34:59 +0300

    decrease button added

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 let counter = document.querySelector("#counter");
-const increase = document.querySelector("#increase");
 const save = document.querySelector(".save");
 const saveEl = document.querySelector(".saved-el");
 
@@ -7,6 +6,10 @@ const saveEl = document.querySelector(".saved-el");
 let count = 0;
 function add() {
     count +=1
+    counter.innerText = count;
+}
+function minus() {
+    count -=1
     counter.innerText = count;
 }
 function saved() {
